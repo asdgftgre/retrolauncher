f9b2070321ed1fc425225126959acbbe3f1f95fe
asdgftgre <todynho196@gmail.com>
2025-03-12 00:34:17 -0300

    Update BootstrapperStyle.cs

--- Bloxstrap/Enums/BootstrapperStyle.cs
+++ Bloxstrap/Enums/BootstrapperStyle.cs
@@ -3,11 +3,9 @@
     public enum BootstrapperStyle
     {
         VistaDialog,
-        LegacyDialog2008,
         LegacyDialog2011,
         ProgressDialog,
         ClassicFluentDialog,
-        ByfronDialog,
         [EnumName(StaticName = "Bloxstrap")]
         FluentDialog,
         FluentAeroDialog,
