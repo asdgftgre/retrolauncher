24a2217e80ca2438527c577b06e833fde50ff791
asdgftgre <todynho196@gmail.com>
2025-03-12 00:33:45 -0300

    Update BootstrapperIcon.cs

--- Bloxstrap/Enums/BootstrapperIcon.cs
+++ Bloxstrap/Enums/BootstrapperIcon.cs
@@ -4,8 +4,6 @@
     {
         [EnumName(StaticName = "Bloxstrap")]
         IconBloxstrap,
-        [EnumName(StaticName = "2008")]
-        Icon2008,
         [EnumName(StaticName = "2011")]
         Icon2011,
         IconEarly2015,
@@ -14,8 +12,6 @@
         Icon2017,
         [EnumName(StaticName = "2019")]
         Icon2019,
-        [EnumName(StaticName = "2022")]
-        Icon2022,
         [EnumName(FromTranslation = "Common.Custom")]
         IconCustom,
         [EnumName(FromTranslation = "Enums.BootstrapperStyle.ClassicFluentDialog")]
