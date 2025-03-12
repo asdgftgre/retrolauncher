2908377c66ad8d823518a728da06c04c7739b859
asdgftgre <todynho196@gmail.com>
2025-03-12 00:34:33 -0300

    Update CursorType.cs

--- Bloxstrap/Enums/CursorType.cs
+++ Bloxstrap/Enums/CursorType.cs
@@ -6,9 +6,6 @@
         [EnumName(FromTranslation = "Common.Default")]
         Default,
 
-        [EnumSort(Order = 3)]
-        From2006,
-
         [EnumSort(Order = 2)]
         From2013
     }
